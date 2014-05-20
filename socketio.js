2e8df95f8735de0c6a1c3c9bc4c94991c2bd80e2
Luke Karrys <lukekarrys@gmail.com>
2014-05-20 13:24:35 -0700

    dont require special signalmaster event to see who is in the room

--- socketio.js
+++ socketio.js
@@ -3,12 +3,14 @@
 var program = require('commander');
 var modulePackage = require('./package');
 var defaults = {
-    server: process.env.SERVER || 'http://signalmaster.dokku.lukekarrys.com'
+    server: process.env.SERVER || 'http://signaling.simplewebrtc.com:8888',
+    room: process.env.ROOM || 'lukes-magical-r2d2-telephone'
 };
 
 program
     .version(modulePackage.version)
     .option('--server [server]', 'Server', String, defaults.server)
+    .option('--room [room]', 'Room', String, defaults.room)
     .parse(process.argv);
 
 
@@ -20,37 +22,53 @@ var R2D2 = require('./index');
 var R2D2ringer = R2D2({
     ascii: false
 });
-var meId;
+var here = [];
 
-var ring = function (ids) {
-    if (ids.length > 0) {
-        R2D2ringer(function () {
-            console.log('Ringing', ids);
-        });
+function ring(event) {
+    R2D2ringer(function () {
+        console.log('Ringing due to', event);
+    });
+}
+
+function removeFromArray(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax = arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
     }
-};
+    return arr;
+}
 
 socket.on('connect', function () {
-    meId = socket.socket.sessionid;
-    console.log('connected', meId);
+    console.log('connected as', socket.socket.sessionid);
 
-    socket.emit('join', 'lukekarrys', function (err, roomDesc) {
+    socket.emit('join', program.room, function (err, roomDesc) {
         // If there are clients in the room when we join
         // other than this listener, attempt to ring
-        var others = _.without(_.keys(roomDesc.clients), meId);
-        ring(others);
+        here = _.keys(roomDesc.clients);
+        console.log('ROOM:', program.room);
+        console.log('CONNECTED:', here.join(', '));
+        if (here.length > 0) {
+            ring(here.join(', '));
+        } else {
+            console.log('Room is empty');
+        }
     });
 
-    socket.on('join', function (data) {
-        var here = data.here;
-        var joinedId = data.id;
-        var others = _.without(here, joinedId, meId);
-
-        // If a client joins other than this listener
-        // And it is the first client, attempt to ring
-        // We only ring for the first client
-        if (joinedId !== meId && others.length === 0) {
-            ring(joinedId);
+    socket.on('message', function (message) {
+        if (message.type === 'offer') {
+            console.log('JOINED:', message.from);
+            if (here.length === 0) {
+                ring(message.from);
+            }
+            here.push(message.from);
         }
+    });
+
+    socket.on('remove', function (message) {
+        console.log('LEFT:', message.id);
+        removeFromArray(here, message.id);
     });
 });
