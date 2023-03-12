94024b1f28a5eebae5eba5fcf401084f879aa2ef
Luke Karrys <luke@lukekarrys.com>
2023-03-12 16:15:18 -0700

    stuff

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 if [ "$1" == "interactive" ]; then 
-  FLAGS="-it"
+  FLAGS="--rm -it"
 else
   FLAGS="-d"
 fi
 
-docker run --rm "$FLAGS" \
+docker run "$FLAGS" \
   -p 5038:5038/tcp \
   -p 5060:5060/udp \
   -p 8000:8000/tcp \
